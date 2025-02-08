813a6607915035739abd191ddf42da0b3e259129
felehdev <felehdev@gmail.com>
2025-02-08 23:07:43 +0100

    chore: fix nodemon.json to get changing from the host machine

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World with ts-node and nodemon 3!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Good ${port}`);
 });
